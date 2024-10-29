e37f06b2ba02ac2710ce8339d868087a260e7861
Wiraama <mawira01@students.uonbi.ac.ke>
2024-10-29 12:09:44 +0300

    first 3

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+eturn the value in self.cache_data linked to key.
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ class to inherit fro, basecaching """
+
+    def put(self, key, item):
+        """ ... """
+        if key == None or item == None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ ... """
+        if not key or key == None:
+            return None
+        return self.cache_data.get(key)
