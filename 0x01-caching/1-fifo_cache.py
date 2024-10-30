53263edb9eab411106d1aff042005e510dde9b93
Wiraama <mawira01@students.uonbi.ac.ke>
2024-10-30 09:23:01 +0300

    1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,25 +1,37 @@
 #!/usr/bin/env python3
-"""  class FIFOCache that inherits from BaseCaching and is a caching system """
-BaseCaching = __import__('base_caching').BaseCaching
+
+'''Task 1: FIFO caching
+'''
+
+
+from collections import OrderedDict
+from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """ ... """
+    '''A class `FIFOCache` that inherits from
+       `BaseCaching` and is a caching system.
+    '''
 
     def __init__(self):
         super().__init__()
-
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
+        '''assign to the dictionary `self.cache_data` the
+           `item` value for the key `key`
+        '''
+
         if key is None or item is None:
             return
-        self.cache_data[key] = item
 
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            discard_data = next(iter(self.cache_data))
-            print(f"DISCARD: {discard_data}")
-            del self.cache_data[discard_data]
+            first_key, _ = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {first_key}")
+
+        self.cache_data[key] = item
 
     def get(self, key):
-        if key is None or key not in self.cache_data:
-            return None
+        '''return the value in `self.cache_data` linked to `key`
+        '''
+        return self.cache_data.get(key, None)
