1a96b14883bc7f850b558fb7787d44120a6c54ae
Wiraama <mawira01@students.uonbi.ac.ke>
2024-10-30 08:28:36 +0300

    0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -8,11 +8,13 @@ BaseCaching = __import__('base_caching').BaseCaching
 class BasicCache(BaseCaching):
     """ class to inherit fro, basecaching """
 
+
     def put(self, key, item):
         """ ... """
         if key is None or item is None:
             return
         self.cache_data[key] = item
+
 
     def get(self, key):
         """ ... """
