a397b0eff0dfad819e2c491447d7abd4af0eb104
Wiraama <mawira01@students.uonbi.ac.ke>
2024-10-30 08:31:19 +0300

    0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -11,9 +11,8 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
         """ ... """
-        if key is None or item is None:
-            return
-        self.cache_data[key] = item
+        if key is not None and item is not None:
+            self.cache_data[key] = item
 
 
     def get(self, key):
